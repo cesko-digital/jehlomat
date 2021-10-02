ed2bb124ca5c1b36f917c9ec568183b87bb4beba
kisto <juraj.kisto@gmail.com>
2021-10-02 10:52:14 +0200

    Basic project structure and routing (#26)
    * added basic conf for unit test
    
    * added basic router navigation
    
    * tests name changes
    
    Co-authored-by: Juraj Kisto <juro.kisztner@gmail.com>

--- web-jehlomat/src/App/App.tsx
+++ web-jehlomat/src/App/App.tsx
@@ -1,8 +1,13 @@
 import React from 'react';
+import Navigation from '../Components/Navigation';
 
 const HelloJehlomat = () => {
   return (
+    <>
+      <Navigation />
       <h3>Hello Jehlomat</h3>
+    </>
+
   );
 };
 
