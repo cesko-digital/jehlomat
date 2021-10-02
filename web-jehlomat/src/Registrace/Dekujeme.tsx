4920f83ff31d591406b1986e3ed250f575f4ed95
Jaroslav Pšenička <jaroslav.psenicka@gmail.com>
2021-10-02 14:31:27 +0200

    JH-12 registrace organizace thank you (#36)
    * #12 test na link
    
    * #12 secondary button, flex

--- web-jehlomat/src/Registrace/Dekujeme.tsx
+++ web-jehlomat/src/Registrace/Dekujeme.tsx
@@ -6,22 +6,34 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faCheckCircle } from '@fortawesome/free-solid-svg-icons';
 import styled from 'styled-components';
 
+import { primary as bgColor, white as textColor } from '../Components/Utils/Colors' 
+import SecondaryButton from '../Components/Buttons/SecondaryButton/SecondaryButton';
+
 const Container = styled.div`
   display: flex;
+  flex: 1;
   flex-direction: column;
   justify-content: center;
   align-items: center;
+  color: ${textColor};
+  background-color: ${bgColor};
+`
+const InfoText = styled.div`
+  margin-top: 2rem;
+  margin-bottom: 2rem;
+  display: flex;
+  text-align: center;
 `
 
 export default function Dekujeme() {
   return (
     <Container>
       <h2>Teď už je to na nás</h2>
-      <FontAwesomeIcon icon={faCheckCircle} size="2x" />
-      <div>Vaši registraci jsme přijali, po schválení vám přijde email
-        s přihlašovacími údaji.</div>
+      <FontAwesomeIcon icon={faCheckCircle} size="3x" />
+      <InfoText>Vaši registraci jsme přijali, po schválení vám přijde email
+        s přihlašovacími údaji.</InfoText>
       <Link to="/">
-        <button>Zpět na přihlášení</button>
+        <SecondaryButton text="Zpět na přihlášení" />
       </Link>
     </Container>
   );
