aa0a83a4c1a43e3020a0a703ccc8b6f9e2a619ca
Juraj Kisto <juro.kisztner@gmail.com>
2021-10-02 09:09:06 +0200

    tests name changes

--- web-jehlomat/src/App/App.spec.tsx
+++ web-jehlomat/src/App/App.spec.tsx
@@ -3,7 +3,7 @@ import { shallow } from "enzyme";
 
 import App from "./App";
 
-it("renders the heading", () => {
+it("Render App", () => {
     const result = shallow(<App />).contains(<h3>Hello Jehlomat</h3>);
     expect(result).toBeTruthy();
 });
--- web-jehlomat/src/Components/Navigation.spec.tsx
+++ web-jehlomat/src/Components/Navigation.spec.tsx
@@ -3,7 +3,7 @@ import { shallow } from "enzyme";
 
 import Navigation from "./Navigation";
 
-it("renders the heading", () => {
+it("Render navigation", () => {
     const result = shallow(<Navigation />);
     expect(result).toBeTruthy();
 });
