bdf324374254894cddbecf204bff25322ffa55e1
Juraj Kisto <juro.kisztner@gmail.com>
2021-10-02 08:33:04 +0200

    added basic router navigation

---
+++ web-jehlomat/src/Components/Navigation.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+import {
+  Link
+} from "react-router-dom";
+
+export default function Navigation() {
+  return (
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/about">About</Link>
+            </li>
+          </ul>
+        </nav>
+      </div>
+  );
+}
--- web-jehlomat/src/index.tsx
+++ web-jehlomat/src/index.tsx
@@ -1,10 +1,26 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+
 import App from "./App/App";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Router>
+      <Switch>
+        <Route path="/about">
+          <div><h1>About</h1></div>
+        </Route>
+        <Route path="/">
+          <App />
+        </Route>
+      </Switch>
+    </Router>
   </React.StrictMode>,
   document.getElementById("root")
 );
