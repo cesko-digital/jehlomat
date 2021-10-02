ccf20d2cfef96d844669c83cdc09639d2339e6df
Jiří Rýdel <jur.aj@seznam.cz>
2021-10-02 12:21:16 +0200

    feat:(JH-12): default colors, components

---
+++ web-jehlomat/src/Components/Colors/Colors.tsx
@@ -0,0 +1,18 @@
+// colors
+export const primary = '#26A69A'
+export const primaryLight = '#64D8CB'
+export const primaryDark = '#00766c'
+
+
+export const secondary = '#FEAB0D'
+export const secondaryLight = '#FFDD4F'
+export const secondaryDark = '#C57C00'
+
+export const black = '#000'
+export const grey = '#787878'
+export const greyLight = '#E0E0E0'
+
+// text
+export const textPrimary = black;
+export const textSecondary = black;
+
---
+++ web-jehlomat/src/Components/Inputs/TextInput/TextInput.tsx
@@ -0,0 +1,9 @@
+import { FC } from "react";
+
+interface TextInput {}
+
+const TextInput: FC<TextInput> = ({}) => {
+  return <div>TextInput</div>;
+};
+
+export default TextInput;
