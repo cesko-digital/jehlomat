67d1938a8b43d6728143c7b6917313b6e3c48395
sleaper <petrik.spac@gmail.com>
2021-10-02 14:03:13 +0200

    ui tweaks

--- web-jehlomat/src/Registrace/Registrace.tsx
+++ web-jehlomat/src/Registrace/Registrace.tsx
@@ -1,19 +1,25 @@
 import React, { FC } from "react";
-import { FormWrapper, Wrapper } from "./styles";
+import {
+  Text,
+  Label,
+  Input,
+  Wrapper,
+  Button,
+  FormItem,
+  FormWrapper,
+} from "./styles";
 import { Formik, Field, Form } from "formik";
 
 interface IRegistrace {}
 
 const Registrace: FC<IRegistrace> = ({}) => {
-  console.log("Test");
   return (
     <Wrapper>
-      <h1>Jehlomat registrace oraganizace</h1>
-
-      <Formik
+      {/* <Formik
         initialValues={{
           orgName: "",
           orgEmail: "",
+          oblast: "",
         }}
         onSubmit={async (values) => {
           await new Promise((r) => setTimeout(r, 500));
@@ -22,20 +28,97 @@ const Registrace: FC<IRegistrace> = ({}) => {
       >
         <Form>
           <FormWrapper>
-            <label htmlFor="orgName">Nazev organizace</label>
-            <Field id="orgName" name="orgName" placeholder="Jane" />
+            <FormItem>
+              <label htmlFor="orgName">Nazev organizace</label>
+              <Field id="orgName" name="orgName" placeholder="Jane" />
+            </FormItem>
 
-            <label htmlFor="orgEmail">Email Organizace</label>
-            <Field
-              id="orgEmail"
-              name="orgEmail"
-              placeholder="jane@acme.com"
-              type="email"
-            />
-            <button type="submit">Registrovat se</button>
+            <FormItem>
+              <label htmlFor="orgEmail">Email Organizace</label>
+              <Field
+                id="orgEmail"
+                name="orgEmail"
+                placeholder="jane@acme.com"
+                type="email"
+              />
+            </FormItem>
+
+            <FormItem>
+              <label htmlFor="oblast">Oblast</label>
+              <Field as="select" name="oblast" id="oblast">
+                <option value="">Vyber</option>
+                <option value="red">Red</option>
+                <option value="green">Green</option>
+                <option value="blue">Blue</option>
+              </Field>
+              <button type="submit">Zalozit</button>
+            </FormItem>
           </FormWrapper>
         </Form>
-      </Formik>
+      </Formik> */}
+
+      <Formik
+        initialValues={{ email: "", password: "", oblast: "" }}
+        onSubmit={(values) => {
+          console.log(values);
+        }}
+        render={({
+          touched,
+          errors,
+          values,
+          handleChange,
+          handleBlur,
+          handleSubmit,
+        }) => (
+          <Form onSubmit={handleSubmit}>
+            <FormWrapper>
+              <FormItem>
+                <Label>
+                  Email *
+                  {touched.email && errors.email && (
+                    <Text color="red">{errors.email}</Text>
+                  )}
+                  <Input
+                    onChange={handleChange}
+                    onBlur={handleBlur}
+                    value={values.email}
+                    type="text"
+                    name="email"
+                    placeholder="Email"
+                  />
+                </Label>
+              </FormItem>
+
+              <FormItem>
+                <Label>
+                  Password *
+                  {touched.password && errors.password && (
+                    <Text color="red">{errors.password}</Text>
+                  )}
+                  <Input
+                    onChange={handleChange}
+                    onBlur={handleBlur}
+                    value={values.password}
+                    type="password"
+                    name="password"
+                    placeholder="Password"
+                  />
+                </Label>
+              </FormItem>
+              <FormItem>
+                <label htmlFor="oblast">Oblast</label>
+                <Field as="select" name="oblast" id="oblast">
+                  <option value="">Vyber</option>
+                  <option value="red">Red</option>
+                  <option value="green">Green</option>
+                  <option value="blue">Blue</option>
+                </Field>
+              </FormItem>
+              <Button type="submit">Zalozit</Button>
+            </FormWrapper>
+          </Form>
+        )}
+      />
     </Wrapper>
   );
 };
--- web-jehlomat/src/Registrace/styles.tsx
+++ web-jehlomat/src/Registrace/styles.tsx
@@ -5,6 +5,7 @@ export const Wrapper = styled.section`
   background: #26a69a;
   text-align: center;
   height: 100%;
+  color: white;
 `;
 
 export const FormWrapper = styled.div`
@@ -12,4 +13,64 @@ export const FormWrapper = styled.div`
   flex-direction: column;
   justify-content: center;
   align-items: center;
+`;
+
+export const FormItem = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  margin: 5;
+  text-align: flex-start;
+`;
+
+export const Form = styled.form`
+  width: 300px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+export const Input = styled.input`
+  width: 300px;
+  height: 35px;
+  background-color: #fff;
+`;
+
+export const Button = styled.button`
+  width: 300px;
+  height: 35px;
+  background-color: #5995ef;
+  color: #fff;
+  border-radius: 3px;
+`;
+
+// Text
+
+export const Title = styled.h1`
+  font-family: "Raleway", sans-serif;
+  font-weight: 600;
+  color: #4d4d4d;
+  font-size: 2.2em;
+`;
+
+export const Title2 = styled.h2`
+  font-family: "Raleway", sans-serif;
+  font-weight: 300;
+  color: #4d4d4d;
+  font-size: 1.8em;
+`;
+
+export const Text = styled.p`
+  font-family: "Raleway", sans-serif;
+  color: ${(props) => props.color || "#4d4d4d"};
+`;
+
+export const Label = styled.label`
+  display: flex;
+  flex-direction: column;
+  text-align: flex-start;
+  color: black;
+  font-family: "Raleway", sans-serif;
+  font-size: 0.8em;
+  margin: 0.5em 0;
 `;
