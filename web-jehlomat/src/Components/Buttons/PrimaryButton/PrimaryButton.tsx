3036212038d3d31dc8b12e836618a9d434e85862
Jiří Rýdel <jur.aj@seznam.cz>
2021-10-02 13:56:24 +0200

    feat:(JH-12): buttons, typography

--- web-jehlomat/src/Components/Buttons/PrimaryButton/PrimaryButton.tsx
+++ web-jehlomat/src/Components/Buttons/PrimaryButton/PrimaryButton.tsx
@@ -1,9 +1,29 @@
 import { FC } from "react";
+import styled from "styled-components";
+import {primaryDark, white} from '../../Utils/Colors'
+import { H4 } from "../../Utils/Typography";
 
-interface PrimaryButton {}
+interface PrimaryButton {
+  text: String;
+}
 
-const PrimaryButton: FC<PrimaryButton> = ({}) => {
-  return <div>PrimaryButton</div>;
+const Button = styled.button`
+  height: 48px;
+  border:none;
+  box-shadow: 0px 4px 5px rgba(0, 0, 0, 0.14), 0px 1px 10px rgba(0, 0, 0, 0.12), 0px 2px 4px rgba(0, 0, 0, 0.2);
+  border-radius: 28px;
+  background-color: ${primaryDark};
+  color: ${white};
+  cursor: pointer;
+  padding: 0px 20px;
+`;
+
+const PrimaryButton: FC<PrimaryButton> = ({ text }) => {
+  return (
+    <Button>
+      <H4>{text}</H4>
+    </Button>
+  );
 };
 
 export default PrimaryButton;
