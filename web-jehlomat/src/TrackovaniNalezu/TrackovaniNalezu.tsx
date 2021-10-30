f88b67389c96437d9121b72e579a5481045f177b
samueldusek <smldsk@protonmail.com>
2021-10-30 13:22:08 +0200

    Update: Edit page styles and fix validation schema

--- web-jehlomat/src/TrackovaniNalezu/TrackovaniNalezu.tsx
+++ web-jehlomat/src/TrackovaniNalezu/TrackovaniNalezu.tsx
@@ -1,5 +1,5 @@
-import { FC } from 'react';
-import { Formik, Form, FormikHelpers } from 'formik';
+import React, { FC } from 'react';
+import { Formik, Form } from 'formik';
 import TextInput from '../Components/Inputs/TextInput/TextInput';
 import PrimaryButton from '../Components/Buttons/PrimaryButton/PrimaryButton';
 import TextButton from '../Components/Buttons/TextButton/TextButton';
@@ -12,71 +12,64 @@ import Container from '@mui/material/Container';
 import Grid from '@mui/material/Grid';
 import Typography from '@mui/material/Typography';
 import { primaryDark } from '../Components/Utils/Colors';
+import TitleBar from '../Components/Navigation/TitleBar';
 
 interface ITrackovaniNalezu {}
 
-interface Values {
-    kod: string;
-}
-
 const validationSchema = yup.object({
-    heslo: yup.string().min(8, 'Trackovaí kód musí mít přesně 8 znaků.').required('Trackovací kód je povivnný.'),
+    kod: yup.string().length(8, 'Trackovaí kód musí mít přesně 8 znaků.').required('Trackovací kód je povivnný.'),
 });
 
 const TrackovaniNalezu: FC<ITrackovaniNalezu> = ({}) => {
     let history = useHistory();
 
-    return (
-        <Container maxWidth="xs">
-            <Box sx={{ height: '100vh' }}>
-                <Grid container direction="column" justifyContent="center" alignItems="center" sx={{ height: '100%' }}>
-                    <Grid container direction="column" justifyContent="center" alignItems="center">
-                        <Typography align="center" variant="h5" color={primaryDark}>
-                            Pro zobrazení stavu nálezu, zadejte prosím trasovací kód.
-                        </Typography>
-                        <Box sx={{ mt: '2rem' }}>
-                            <Formik
-                                initialValues={{ kod: '' }}
-                                validationSchema={validationSchema}
-                                onSubmit={async (values: Values, { setErrors }) => {
-                                    try {
-                                        const response: AxiosResponse<any> = await API.post('/api/v1/jehlomat/syringe', values);
-                                        const status = response.status;
+    const handleOnClickBackButton = (event: React.MouseEvent<HTMLButtonElement>) => {
+        history.goBack();
+    };
 
-                                        // TODO
-                                    } catch (error: any) {
-                                        //link to error page
-                                    }
-                                }}
-                            >
-                                {({ handleSubmit, touched, handleChange, handleBlur, values, errors, isValid }) => {
-                                    return (
-                                        <Form onSubmit={handleSubmit}>
-                                            <Grid container direction="column" justifyContent="center" alignItems="center">
-                                                <TextInput
-                                                    id="kod"
-                                                    onChange={handleChange}
-                                                    onBlur={handleBlur}
-                                                    value={values.kod}
-                                                    type="text"
-                                                    name="kod"
-                                                    label="Trasovací kód nálezu"
-                                                    required={true}
-                                                    error={touched.kod && Boolean(errors.kod) ? errors.kod : undefined}
-                                                />
-                                                <Box sx={{ mt: '3rem', mb: '1rem' }}>
-                                                    <PrimaryButton id="submit" text="Potvrdit" type="submit" disabled={!isValid} />
-                                                </Box>
-                                            </Grid>
-                                        </Form>
-                                    );
-                                }}
-                            </Formik>
-                        </Box>
-                        <TextButton text="Zpět" />
-                    </Grid>
+    return (
+        <Container maxWidth="xs" sx={{ height: '100vh' }}>
+            <Grid container direction="column" justifyContent="center" alignItems="center" sx={{ height: '100%' }}>
+                <TitleBar></TitleBar>
+                <Grid container direction="column" justifyContent="center" alignItems="center">
+                    <Typography align="center" variant="body1" fontWeight="bold" color={primaryDark}>
+                        Pro zobrazení stavu nálezu, zadejte prosím trasovací kód.
+                    </Typography>
+                    <Box sx={{ mt: '2rem', width: '100%' }}>
+                        <Formik
+                            initialValues={{ kod: '' }}
+                            validationSchema={validationSchema}
+                            onSubmit={values => {
+                                console.log(values);
+                            }}
+                        >
+                            {({ handleSubmit, touched, handleChange, handleBlur, values, errors, isValid }) => {
+                                return (
+                                    <Form onSubmit={handleSubmit}>
+                                        <Grid container direction="column" justifyContent="center" alignItems="center">
+                                            <TextInput
+                                                id="kod"
+                                                onChange={handleChange}
+                                                onBlur={handleBlur}
+                                                value={values.kod}
+                                                type="text"
+                                                name="kod"
+                                                label="Trasovací kód nálezu"
+                                                required={true}
+                                                error={touched.kod && Boolean(errors.kod) ? errors.kod : undefined}
+                                            />
+                                            <Box sx={{ mt: '3rem', mb: '1rem' }}>
+                                                <PrimaryButton id="submit" text="Potvrdit" type="submit" disabled={!isValid} />
+                                            </Box>
+                                        </Grid>
+                                    </Form>
+                                );
+                            }}
+                        </Formik>
+                    </Box>
+                    <TextButton text="Zpět" type="button" onClick={handleOnClickBackButton} />
                 </Grid>
-            </Box>
+            </Grid>
         </Container>
     );
 };
