b9581e267fc12932272f52b8f682844eac731fc4
Katerina Volakova <volakova.kat@gmail.com>
2021-11-26 19:13:48 +0100

    render mobile UI

--- web-jehlomat/src/App/App.tsx
+++ web-jehlomat/src/App/App.tsx
@@ -1,18 +1,54 @@
-import React from "react";
+import React, { FunctionComponent } from 'react';
 import PrimaryButton from "../Components/Buttons/PrimaryButton/PrimaryButton";
 import StoryBook from "../Components/StoryBook";
 import { Header } from '../Components/Header/Header';
 import { Footer } from '../Components/Footer/Footer';
+import { HeaderMobile } from '../Components/Header/HeaderMobile';
+import { ValidationMap } from 'prop-types';
+import { MaxMobileWidth } from '../Components/Utils/Constants';
 
-const HelloJehlomat = () => {
-  return (
-    <>
-        <Header/>
-        <h3>Hello Jehlomat</h3>
-        <StoryBook/>
-        <Footer />
-    </>
-  );
-};
+class HelloJehlomat extends React.Component {
+
+    constructor(props: any) {
+        super(props);
+        this.state = {
+            width: window.innerWidth,
+        };
+    }
+
+    componentDidMount() {
+        window.addEventListener('resize', this.handleWindowSizeChange);
+    }
+
+    componentWillUnmount() {
+        window.removeEventListener('resize', this.handleWindowSizeChange);
+    }
+
+    handleWindowSizeChange = () => {
+        this.setState({ width: window.innerWidth });
+    };
+
+    renderPage() {
+        const isMobile = window.innerWidth <= MaxMobileWidth;
+        if (isMobile) {
+            return (
+                <HeaderMobile />
+            )
+        } else {
+            return (<>
+                <Header />
+                <h3>Hello Jehlomat</h3>
+                <StoryBook/>
+                <Footer />
+                </>
+            )
+        }
+    }
+
+    render(){
+        return this.renderPage();
+    }
+
+}
 
 export default HelloJehlomat;
