4b19e4abd7c9c0dadcab712b9d89b704fe321826
Tomas Fechtner <fechtnertomas@gmail.com>
2022-02-04 20:22:35 +0100

    fix db params (#110)

--- infrastructure/aws/variables.tf
+++ infrastructure/aws/variables.tf
@@ -27,18 +27,6 @@ variable "development-public-domain" {
   type    = string
 }
 
-variable "database-host" {
-  type = string
-}
-
-variable "database-port" {
-  type = string
-}
-
-variable "database-name" {
-  type = string
-}
-
 variable "database-username" {
   type = string
 }
