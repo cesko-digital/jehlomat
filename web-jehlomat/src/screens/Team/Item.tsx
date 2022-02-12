dcc732295b6bd98b079d323535aa245303c3632d
kisto <juraj.kisztner@dxc.com>
2022-02-12 21:00:18 +0100

    added chimp and remove item

--- web-jehlomat/src/screens/Team/Item.tsx
+++ web-jehlomat/src/screens/Team/Item.tsx
@@ -1,8 +1,17 @@
+import { Chip } from "@mui/material";
+import styled from '@emotion/styled';
 
+const ChipParent = styled.li`
+    margin: 0;
+    padding: 0.5em;
+    text-align: left;
+`;
 
-const Item = (props: { data: string}) => {
-    return(
-        <div>{props.data}</div>
+const Item = (props: { id: string, name: string, remove:any }) => {
+    return (
+        <ChipParent>
+            <Chip label={props.name} variant="outlined" onDelete={()=>{props.remove(props.id)}} />
+        </ChipParent>
     )
 }
 
