f44cc4e5a897fd316a61cb5816d816a95ec4e5e1
Juraj Kisztner <juro.kisztner@gmail.com>
2022-03-07 13:18:31 +0100

    Seed for teams

---
+++ web-jehlomat/src/config/user.ts
@@ -0,0 +1,31 @@
+import jwt_decode from "jwt-decode";
+import { AxiosResponse } from 'axios';
+import API from './baseURL';
+
+interface IToken {
+    "aud": string;
+    "user-id": string;
+    "iss": string;
+    "exp": string;
+}
+
+interface IResponse {
+    id: number;
+    email?: string;
+    username: string;
+    password?: string;
+    organizationId: number;
+    teamId: number;
+    isAdmin?: boolean;
+    verified?: boolean;
+}
+
+export const getMe = (token: string) => {
+    const decoded: IToken = jwt_decode(token);
+    return decoded["user-id"];
+}
+export const getUser = async (token: string) => {
+    const response: AxiosResponse<IResponse> = await API.get("/api/v1/jehlomat/user/" + getMe(token));
+    return response.data;
+}
+
