cb63eddce62336df18923ac19345edfecf6e1732
Roman Rajchert <rajchert.roman@gmail.com>
2022-03-16 22:36:04 +0100

    chore: adress type

--- web-jehlomat/src/screens/NovyNalez/components/types.ts
+++ web-jehlomat/src/screens/NovyNalez/components/types.ts
@@ -1,7 +1,30 @@
-
 export enum LocationState {
     CHECKING = 'CHECKING',
     CHECKED = 'CHECKED',
     GRANTED = 'GRANTED',
+}
 
+export interface Address {
+    place_id: number;
+    licence: string;
+    osm_type: string;
+    osm_id: number;
+    boundingbox: [string, string, string, string];
+    lat: string;
+    lon: string;
+    display_name: string;
+    class: string;
+    type: string;
+    importance: number;
+    address: {
+        road: string;
+        suburb: string;
+        city: string;
+        municipality: string;
+        county: string;
+        state: string;
+        country: string;
+        postcode: string;
+        country_code: string;
+    };
 }
