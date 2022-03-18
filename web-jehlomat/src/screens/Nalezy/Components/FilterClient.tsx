e86fab2b91a3add9859bc66e75c9c7b26793a72c
Sebastian Busek <ja@sebastianbusek.cz>
2022-03-19 00:08:31 +0100

    feat(JH-46): WIP

---
+++ web-jehlomat/src/screens/Nalezy/Components/FilterClient.tsx
@@ -0,0 +1,25 @@
+﻿import React, {FunctionComponent, useState} from "react";
+import {Filter, Input, Select, SelectItem, ZoomIcon} from "./Filter";
+
+const FilterClient: FunctionComponent = () => {
+  const [organization, setOrganization] = useState<SelectItem | undefined>();
+
+  return (
+    <Filter title="Zadavatel nálezu">
+      <Input>
+        <ZoomIcon />
+      </Input>
+      <Select
+        value={organization}
+        onSelect={item => setOrganization(item)}
+        items={[
+          { text: 'Organizace', value: 1 },
+          { text: 'Tým', value: 2 },
+          { text: 'Jednotlivec', value: 3 },
+        ]}
+      />
+    </Filter>
+  );
+};
+
+export default FilterClient;
