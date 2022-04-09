9400444a14f18164fb731b8c494852b2780db24a
Sebastian Busek <ja@sebastianbusek.cz>
2022-04-09 23:11:42 +0200

    feat(JH-46): leftovers

--- web-jehlomat/src/screens/Nalezy/Components/utils/isFinalPath.ts
+++
@@ -1,9 +0,0 @@
-﻿const isFinalPath = (value: string): boolean => {
-    const { pathname } = window.location;
-    const parts = pathname.split('/');
-    const last = parts[Math.max(parts.length - 1, 0)];
-
-    return last.toLocaleLowerCase() === value.toLocaleLowerCase();
-};
-
-export default isFinalPath;
