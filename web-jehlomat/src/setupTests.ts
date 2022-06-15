33f2d2c51a21bee701e40e9a2144af41230cdfef
Juraj Kisztner <juro.kisztner@gmail.com>
2022-06-15 06:30:58 +0200

    Enzyme React 17 adapter fix

--- web-jehlomat/src/setupTests.ts
+++ web-jehlomat/src/setupTests.ts
@@ -4,6 +4,6 @@
 // learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom';
 import enzyme from 'enzyme';
-import Adapter from 'enzyme-adapter-react-16';
+import Adapter from '@wojtekmaj/enzyme-adapter-react-17';
 
 enzyme.configure({ adapter: new Adapter() });
