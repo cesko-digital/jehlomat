d04a49704d1e33ae20f81a62580a1501936a62c0
mattesCZ <osmmtb@gmail.com>
2022-07-22 15:45:50 +0200

    feat(JH-117): Simplify error logic and reuse main components

--- web-jehlomat/src/screens/Nalezy/Detail.tsx
+++ web-jehlomat/src/screens/Nalezy/Detail.tsx
@@ -2,7 +2,7 @@ import React, { useCallback, useEffect, useState } from 'react';
 import { useHistory, useParams } from 'react-router-dom';
 import { TileLayer } from 'react-leaflet';
 import dayjs from 'dayjs';
-import { Alert, Box, Container } from '@mui/material';
+import { Alert, Box, Container, useMediaQuery } from '@mui/material';
 import { styled } from '@mui/system';
 import { useRecoilValue } from 'recoil';
 import Page from 'screens/Nalezy/Components/Page';
@@ -15,6 +15,7 @@ import RoundButton from 'screens/Nalezy/Components/RoundButton';
 import { DEFAULT_POSITION, DEFAULT_ZOOM_LEVEL } from 'screens/Nalezy/NovyNalez/constants';
 import LeafletMap from 'screens/Nalezy/Components/LeafletMap';
 import { primary } from 'utils/colors';
+import { media } from 'utils/media';
 import { Loader } from 'utils/Loader';
 import { Syringe, SyringeChangeReq } from 'screens/Nalezy/types/Syringe';
 import { isStatusSuccess } from 'utils/payload-status';
@@ -49,6 +50,7 @@ const formatDate = (date: number | undefined): string => (date ? dayjs(date * 10
 
 const Detail = () => {
     const [loader, setLoader] = useState<Loader<Syringe>>({});
+    const isMobile = useMediaQuery(media.lte('mobile'));
 
     const history = useHistory();
     const { id } = useParams<{ id: string }>();
@@ -100,7 +102,44 @@ const Detail = () => {
 
     const loading = loader.resp === undefined && loader.err === undefined;
     const error = loader.resp === undefined && loader.err !== undefined;
-    const data: Syringe | undefined = loader.resp;
+    const data: Syringe | undefined = error ? undefined : loader.resp;
+
+    const DetailsCmp = (
+        <Details>
+            <TextInput label={texts.DETAIL__SYRINGES_COUNT} value={data?.count} disabled />
+            <TextInput label={texts.DETAIL__DATETIME} value={formatDate(data?.createdAt)} disabled />
+            <TextInput label={texts.DETAIL__PLACE} value={data?.location?.obec} disabled />
+            <TextInput label={texts.DETAIL__NOTE} value={data?.note} disabled />
+            <TextInput label={texts.DETAIL__STATE} value={state(data)} disabled />
+            {data && !data.demolished && !data.reservedBy && (
+                <Box display="flex" alignItems="center" flexDirection="column" py={2}>
+                    <TextButton color={primary} onClick={() => reserve(data)} text="Nález si rezervuji k pozdější likvidaci" textTransform="uppercase" textDecoration="underline" />
+                </Box>
+            )}
+        </Details>
+    );
+
+    const MapCmp = (
+        <Box height={443} borderRadius={2} overflow="hidden" zIndex={0}>
+            <LeafletMap preferCanvas center={DEFAULT_POSITION} zoom={DEFAULT_ZOOM_LEVEL}>
+                {data && (
+                    <Pin syringe={data}>
+                        <PinMenu closeButton={false} minWidth={220}>
+                            <Info>
+                                <Location>{data.location.obec}</Location>
+                                <Time>{formatDate(data.createdAt)}</Time>
+                                <State>
+                                    <PreviewSyringeState syringe={data} />
+                                </State>
+                            </Info>
+                            <Links onClose={load} syringe={data} />
+                        </PinMenu>
+                    </Pin>
+                )}
+                <TileLayer attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors' url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
+            </LeafletMap>
+        </Box>
+    );
 
     return (
         <>
@@ -119,59 +158,18 @@ const Detail = () => {
                             <Loading />
                         </Box>
                     )}
-                    {(data || error) && (
-                        <TwoColumns
-                            left={
-                                <Details>
-                                    {error && (
-                                        <Box>
-                                            <Alert severity="error">{texts.DETAIL__NOT_FOUND}</Alert>
-                                        </Box>
-                                    )}
-                                    <TextInput label={texts.DETAIL__SYRINGES_COUNT} value={data?.count} disabled />
-                                    <TextInput label={texts.DETAIL__DATETIME} value={formatDate(data?.createdAt)} disabled />
-                                    <TextInput label={texts.DETAIL__PLACE} value={data?.location?.obec} disabled />
-                                    <TextInput label={texts.DETAIL__NOTE} value={data?.note} disabled />
-                                    <TextInput label={texts.DETAIL__STATE} value={state(data)} disabled />
-                                    {data && !data.demolished && !data.reservedBy && (
-                                        <Box display="flex" alignItems="center" flexDirection="column" py={2}>
-                                            <TextButton
-                                                color={primary}
-                                                onClick={() => reserve(data)}
-                                                text="Nález si rezervuji k pozdější likvidaci"
-                                                textTransform="uppercase"
-                                                textDecoration="underline"
-                                            />
-                                        </Box>
-                                    )}
-                                </Details>
-                            }
-                            right={
-                                <Box height={443} borderRadius={2} overflow="hidden">
-                                    <LeafletMap preferCanvas center={DEFAULT_POSITION} zoom={DEFAULT_ZOOM_LEVEL}>
-                                        {data && (
-                                            <Pin syringe={data}>
-                                                <PinMenu closeButton={false} minWidth={220}>
-                                                    <Info>
-                                                        <Location>{data.location.obec}</Location>
-                                                        <Time>{formatDate(data.createdAt)}</Time>
-                                                        <State>
-                                                            <PreviewSyringeState syringe={data} />
-                                                        </State>
-                                                    </Info>
-                                                    <Links onClose={load} syringe={data} />
-                                                </PinMenu>
-                                            </Pin>
-                                        )}
-                                        <TileLayer
-                                            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-                                            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-                                        />
-                                    </LeafletMap>
-                                </Box>
-                            }
-                        />
+                    {error && (
+                        <Box>
+                            <Alert severity="error">{texts.DETAIL__NOT_FOUND}</Alert>
+                        </Box>
                     )}
+                    {data && isMobile && (
+                        <>
+                            {DetailsCmp}
+                            {MapCmp}
+                        </>
+                    )}
+                    {data && !isMobile && <TwoColumns left={DetailsCmp} right={MapCmp} />}
                 </Container>
             </Page>
         </>
