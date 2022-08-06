5c7c962bb2ff0ab88b784c6826c382cdf07e4bfd
Marek Lisy <marek.lisy.hk@gmail.com>
2022-08-06 21:31:25 +0200

    fix lint error after npm run fix

--- web-jehlomat/src/utils/url.ts
+++ web-jehlomat/src/utils/url.ts
@@ -8,12 +8,12 @@ export function convertSearchStringToMap(params: string): URLSearchParams {
 }
 
 export function dataURLtoFile(dataurl: string, filename: string) {
+    let arr = dataurl.split(',');
     // @ts-ignore
-    var arr = dataurl.split(','),
-        mime = arr[0].match(/:(.*?);/)[1],
-        bstr = atob(arr[1]),
-        n = bstr.length,
-        u8arr = new Uint8Array(n);
+    let mime = arr[0].match(/:(.*?);/)[1];
+    let bstr = atob(arr[1]);
+    let n = bstr.length;
+    let u8arr = new Uint8Array(n);
     while (n--) {
         u8arr[n] = bstr.charCodeAt(n);
     }
