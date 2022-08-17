f038d6e64b5946e738b649a246360bea5e10dba8
Dusan Sacha <sacha@net-wings.eu>
2022-08-17 21:43:48 +0200

    JH-204

--- web-jehlomat/src/screens/Nalezy/NovyNalez/screens/ZadatNalezMapa.tsx
+++ web-jehlomat/src/screens/Nalezy/NovyNalez/screens/ZadatNalezMapa.tsx
@@ -27,7 +27,7 @@ const StyledContainer = styled.div`
         // compensate parent padding, nasty but easiest
         width: 100vw;
         transform: translateX(-16px);
-        height: 80vh;
+        height: 100vh;
     }
 `;
 
