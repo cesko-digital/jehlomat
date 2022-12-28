1deb0e3268a15035725ed08332348fd34e284ef7
Jan Schramhauser <jan.schramhauser@aveco.com>
2022-12-28 10:53:56 +0100

    fix ulozeni nalezu prihlasenych uzivatelu jako zlikvidovano

--- service-jehlomat/src/main/kotlin/api/SyringeApi.kt
+++ service-jehlomat/src/main/kotlin/api/SyringeApi.kt
@@ -88,10 +88,18 @@ fun Route.syringeApi(database: DatabaseService, jwtManager: JwtManager, mailer: 
             post {
                 val syringeRequest = call.receive<SyringeCreateRequest>()
 
-                val userCreatedBy = jwtManager.getLoggedInUserOptional(call, database)
                 val location = database.selectOrInsertLocation(syringeRequest.gps_coordinates)
 
-                val syringe = syringeRequest.toSyringe(location, userCreatedBy?.toUserInfo())
+                val userCreatedBy = jwtManager.getLoggedInUserOptional(call, database)
+                var syringe = syringeRequest.toSyringe(location, userCreatedBy?.toUserInfo())
+                if (userCreatedBy != null) {
+                   syringe = syringe.copy(
+                        demolished = true,
+                        demolisherType = Demolisher.USER,
+                        demolishedBy = userCreatedBy.toUserInfo(),
+                        demolishedAt = Instant.now().epochSecond
+                    )
+                }
                 val syringeId = database.insertSyringe(syringe)
                 if (syringeId == null) {
                     call.respond(
